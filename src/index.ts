4456da820396db6750e5037ce427d785b119e098
Darryl Walker <darryljwalker@me.com>
2024-08-05 08:23:23 +0100

    chore: run on startup

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,17 @@ import { CronJob } from "cron";
 import { backup } from "./backup";
 import { env } from "./env";
 
+if (env.RUN_ON_STARTUP || env.SINGLE_SHOT_MODE) {
+  console.log("Running on start backup...");
+
+  backup();
+}
+
 const job = new CronJob(env.BACKUP_CRON_SCHEDULE, async () => {
   try {
     await backup();
   } catch (error) {
-    console.error("Error while creating backup: ", error)
+    console.error("Error while creating backup: ", error);
   }
 });
 
